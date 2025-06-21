1b3742b6d70e22bff3d98f7a4fa0963992659c05
Libardo-Navarro <josenavarrop05@gmail.com>
2025-06-21 13:48:40 -0500

    add buttons

--- js/diccionario.js
+++ js/diccionario.js
@@ -5,10 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function renderDiccionario() {
   const lista = document.getElementById('lista-diccionario');
+  lista.innerHTML = '';
   diccionarioDatos.forEach(d => {
     const item = document.createElement('div');
     item.className = 'diccionario-item';
-    item.innerHTML = `<div class="term">${d.termino}</div><div class="desc">${d.definicion}</div>`;
+    item.innerHTML = `
+      <div class="term">${d.termino}</div>
+      <div class="desc">${d.definicion}</div>
+      <div class="actions">
+        <button class="btn edit-btn" title="Editar">
+          <img src="icons/edit_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="Editar" />
+        </button>
+        <button class="btn delete-btn" title="Eliminar">
+          <img src="icons/cancel_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="Eliminar" />
+        </button>
+      </div>
+    `;
     lista.appendChild(item);
   });
 }
